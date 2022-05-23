0429bd7f438dd7fde7a30794e0bf9e2b9b13f8ca
Danny L <dllenselink@gmail.com>
2022-05-23 18:01:40 -0500

    Uninstalled clsx

--- src/NavTabs/NavTabs.jsx
+++ src/NavTabs/NavTabs.jsx
@@ -1,62 +1,28 @@
 import './NavTabs.scss';
-import { useNavigate, useLocation } from 'react-router-dom';
-import clsx from 'clsx';
+import { useNavigate } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 
 export const NavTabs = () => {
   const navigate = useNavigate();
-  const location = useLocation();
   const handleNavigate = (path) => () => {
     navigate(path || '/');
   };
 
-  console.log(location);
-
   return (
     <Nav className="justify-content-center">
-      <Nav.Item
-        onClick={handleNavigate('/home')}
-        className={clsx({
-          'nav-item': true,
-          active: location.pathname === '/home',
-        })}
-      >
+      <Nav.Item onClick={handleNavigate('/home')}>
         <Nav.Link>Home</Nav.Link>
       </Nav.Item>
-      <Nav.Item
-        onClick={handleNavigate('/about')}
-        className={clsx({
-          'nav-item': true,
-          active: location.pathname === '/about',
-        })}
-      >
+      <Nav.Item onClick={handleNavigate('/about')}>
         <Nav.Link>About</Nav.Link>
       </Nav.Item>
-      <Nav.Item
-        onClick={handleNavigate('/portfolio')}
-        className={clsx({
-          'nav-item': true,
-          active: location.pathname === '/portfolio',
-        })}
-      >
+      <Nav.Item onClick={handleNavigate('/portfolio')}>
         <Nav.Link>Portfolio</Nav.Link>
       </Nav.Item>
-      <Nav.Item
-        onClick={handleNavigate('/resume')}
-        className={clsx({
-          'nav-item': true,
-          active: location.pathname === '/resume',
-        })}
-      >
+      <Nav.Item onClick={handleNavigate('/resume')}>
         <Nav.Link>Resume</Nav.Link>
       </Nav.Item>
-      <Nav.Item
-        onClick={handleNavigate('/contact')}
-        className={clsx({
-          'nav-item': true,
-          active: location.pathname === '/contact',
-        })}
-      >
+      <Nav.Item onClick={handleNavigate('/contact')}>
         <Nav.Link>Contact</Nav.Link>
       </Nav.Item>
     </Nav>
